2d6e91c5b7275a95d351b567208de544fdfb27a0
laharinara <naralahari4277@gmail.com>
2023-07-24 22:20:04 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def srujana(request):
+    return HttpResponse('<h1>srujana tinnava ra....</h1>')
+def meghana(request):
+    return HttpResponse('<h1 mequeen>my name is meghana sir...</h1>')
+    
